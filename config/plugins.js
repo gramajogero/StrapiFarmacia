df7c119484b714299c6fac183efbe3aac612b08b
Gerónimo Gramajo <geronimo23.mg@gmail.com>
2024-03-25 12:30:51 -0300

    asddd

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,12 @@
 module.exports = ({ env }) => ({
-    "vercel-deploy": {
-      enabled: true,
-      config: {
-        deployHook:
-          "https://api.vercel.com/v1/integrations/deploy/prj_tnzyYKfZW7eYZLWKlCIRWGNdP0bu/B5PmoB6ZFu",
-        apiToken: "IuxcJDjnv8CiP0AIr9BK7hsH",
-        appFilter: "farmacia-strapi",
-        teamFilter: "farmacia-strapi",
-        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-      },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_tnzyYKfZW7eYZLWKlCIRWGNdP0bu/B5PmoB6ZFu",
+      apiToken: "IuxcJDjnv8CiP0AIr9BK7hsH",
+      appFilter: "farmacia-strapi",
+      teamFilter: "GeroGramajo's projects", // Aquí se especifica el nombre del equipo en Vercel
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
-  });
+  },
+});
