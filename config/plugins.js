efefc46861516094a1075e4934658616c26ca1b0
Gerónimo Gramajo <geronimo23.mg@gmail.com>
2024-03-25 12:16:30 -0300

    werwer

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
       config: {
         deployHook:
           "https://api.vercel.com/v1/integrations/deploy/prj_tnzyYKfZW7eYZLWKlCIRWGNdP0bu/B5PmoB6ZFu",
-        apiToken: "yJr3jSzJrLpc2ITzNMbxGdjb",
+        apiToken: "IuxcJDjnv8CiP0AIr9BK7hsH",
         appFilter: "Farmacia",
         teamFilter: "Farmacia-Corrientes",
         roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
