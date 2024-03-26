4a6261db69c5dabf975207ca6f050ae03c8943ce
Gerónimo Gramajo <geronimo23.mg@gmail.com>
2024-03-26 10:57:51 -0300

    asd

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ["IlcLGcvdmQarlpMSjWkm4Q","3qEfXQqWQ1q2snQxBg760g","kyx0y48C89oWpRNmwzyI0g","wpLBR+eI1VeC4Lxlff6unw"]
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
