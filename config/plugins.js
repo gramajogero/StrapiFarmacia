45bda9b748a91686bbd01cd0553c0ad76082720c
Gerónimo Gramajo <geronimo23.mg@gmail.com>
2024-03-26 08:54:42 -0300

    asd

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,16 @@
-module.exports = ({ env }) => ({
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_tnzyYKfZW7eYZLWKlCIRWGNdP0bu/B5PmoB6ZFu",
-      apiToken: "IuxcJDjnv8CiP0AIr9BK7hsH",
-      appFilter: "farmacia-strapi",
-      teamFilter: "GeroGramajo's projects", // Aquí se especifica el nombre del equipo en Vercel
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   "netlify-deployments": {
+//     enabled: true,
+//     config: {
+//       accessToken: "nfp_M5M334ktoMzkXhE8wwMUe1fYbc8xz3Vcdf44",
+//       sites: [
+//         {
+//           name: 'glistening-sopapillas-e88d8f',
+//           id: "1a4b545c-bb59-419a-9834-3aa501d991ac",
+//           buildHook: "https://api.netlify.com/build_hooks/66019e335e06bc0849eaa0d2",
+//           branch: 'master' // optional
+//         }
+//       ]
+//     },
+//   },
+// });
